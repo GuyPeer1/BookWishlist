54254990b99a0c868d95fe0fddf7162b54d43285
Guy Peer <peer.g2@gmail.com>
2023-02-05 15:20:03 +0200

    book-list cmp updated

--- src/cmps/book-list.jsx
+++ src/cmps/book-list.jsx
@@ -2,14 +2,6 @@ import BookListPreview from "./book-list-preview.jsx"
 
 export function BookList({ books }) {
 
-    function ToggleWishList(book) {
-        const book = structuredClone(order)
-        const { startDate, endDate } = range.range1
-        orderToSave.startDate = startDate
-        orderToSave.endDate = endDate
-        updateOrder(orderToSave)
-    }
-
     if (!books) return 'Loading....'
 
     return <section className="book-list">
