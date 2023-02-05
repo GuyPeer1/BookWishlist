baeea9bf3dae269944c540c1f1f4d1bcf80efaeb
ofek ashkenazi <ofeka18@gmail.com>
2023-02-05 14:22:48 +0200

    add useEffct

--- src/views/book-wish-index.jsx
+++ src/views/book-wish-index.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react"
+
 export function BookWishIndex() {
+
+    useEffect(() => {
+        console.log('hi')
+    },[])
+
     return (
         <section className="book-wish-index">
             <h2>hi</h2>
